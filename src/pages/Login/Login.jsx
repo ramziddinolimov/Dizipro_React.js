8a8bba1c44cb81eb0c08b6fdea51cf2541ade4e4
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-12-04 22:02:10 +0500

    add bee image

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,13 @@
 import Header from "../../components/Header/Header";
 import "./Login.scss";
+import BeeImage from "../../assets/images/bee.png"
 
 export default function Login () {
     return (
         <div className="login">
             <Header />
+
+            <img src={BeeImage} alt="" />
         </div>
     )
 }
