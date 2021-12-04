2f0a8e81b1a7f717dca6999dce7b472da6b3d689
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-12-04 22:52:39 +0500

    login add new user to

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -7,11 +7,11 @@ export default function Login () {
         <div className="login">
             <Header />
 
-            <section>
-            <img className="login_bee-image" src={BeeImage} alt="" />
-            <form className="login_form">
-                <h2 className="login_title">Log in</h2>
-            </form>
+            <section className="login_main-section">
+                <img className="login_bee-image" src={BeeImage} alt="" />
+                <form className="login_form">
+                    <h2 className="login_title">Log in</h2>
+                </form>
             </section>
         </div>
     )
