cc962a9764db21e26a60ca4c3ff462979a51169b
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-12-05 23:24:34 +0500

    input export delete

--- src/components/Input/input.jsx
+++ src/components/Input/input.jsx
@@ -1,10 +1,10 @@
 import "./input.scss";
 
 
-export default function Input (props) {
-    return (
-        <label>
-            <input className="input" {...props} />;
-        </label>
-    )
-}
+// export default function Input (props) {
+//     return (
+//         <label>
+//             <input className="input" {...props} />;
+//         </label>
+//     )
+// }
