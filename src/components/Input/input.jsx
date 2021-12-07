654769b5b0dd6df96d8795f1d034b18beb813046
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-12-07 22:43:21 +0500

    input wrapper add

--- src/components/Input/input.jsx
+++ src/components/Input/input.jsx
@@ -4,7 +4,10 @@ import "./input.scss";
 export default function Input (props) {
     return (
         <div className="input_wrapper">
-            <input className="input" {...props} autoComplete="off" />
+            <input className={`input ${props.className}`} 
+                {...props} 
+                autoComplete="off" 
+            />
             <label data-placeholder={props.placeholder}></label>
         </div>
     )
